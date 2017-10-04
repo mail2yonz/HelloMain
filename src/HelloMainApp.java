cbf42691bf2ed132819c859871f707e1dbab0891
GraphicStarr <mail2yonz@gmail.com>
2017-10-04 10:09:56 -0400

    Hello commit

---
+++ src/HelloMainApp.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class HelloMainApp {
+
+
+    public static void main(String[] args)
+    {
+         String thisname;
+        Scanner scan = new Scanner (System.in);
+        System.out.println("Enter the persons name:");
+        thisname= scan.next ();
+
+        System.out.println(Hello ( thisname ));
+
+
+    }
+
+    public static String Hello(String name){
+
+
+        return "Hello " + name;
+    }
+}
